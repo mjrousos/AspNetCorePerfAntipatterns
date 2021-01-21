002bc99051c3611ca1a3a478d9d5163a1e66b115
Mike Rousos <Mike@MJRousos.com>
2021-01-21 09:23:11 -0500

    Add a stack overflow scenario to CombinedDemo

---
+++ src/CombinedDemo/Controllers/SumController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace CombinedDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SumController : ControllerBase
+    {
+        [HttpGet("{input}")]
+        public ActionResult<long> GetSumOneToInput([FromRoute] int input)
+        {
+            // This should, of course, guard against negative inputs. But supposing it doesn't...
+            // if (input < 0)
+            // {
+            //     return BadRequest("Input must be greater than or equal to zero");
+            // }
+
+            return Ok(CalculateSum(input));
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private long CalculateSum(long input) =>
+            input switch
+            {
+                0 => 0,
+                _ => CalculateSum(input - 1) + input
+            };
+    }
+}
